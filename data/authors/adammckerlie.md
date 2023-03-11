32562f59f72b8507c85145b2941128b5e1d3f47f
Adam McKerlie <adammckerlie@gmail.com>
2023-03-10 21:53:20 -0500

    New Engineering blog

---
+++ data/authors/adammckerlie.md
@@ -0,0 +1,9 @@
+---
+name: Adam McKerlie
+avatar: /static/avatars/adammckerlie.png
+occupation: Director of Engineering
+twitter: https://twitter.com/adammckerlie
+linkedin: https://www.linkedin.com/adammckerlie
+---
+
+Some text
---
+++ data/headerNavLinks.js
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/tags/web', title: 'Web' },
+  { href: '/tags/infrastructure', title: 'Infrastructure' },
+  { href: '/tags/open-source', title: 'Open Source' },
+  { href: '/tags', title: 'Tags' },
+  { href: 'https://sentry.io/changelog/', title: 'Changelog' },
+]
+
+export default headerNavLinks
