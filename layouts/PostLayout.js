3a17c8efb62c9efa488b805bc5f160f0a650a54e
Adam McKerlie <adammckerlie@gmail.com>
2023-04-02 07:34:10 -0400

    Fix prettier errors

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -9,7 +9,6 @@ import Comments from '@/components/comments'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 import SocialIcon from '@/components/social-icons'
 
-
 const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
 const discussUrl = (slug) =>
   `https://mobile.twitter.com/search?q=${encodeURIComponent(
@@ -18,16 +17,17 @@ const discussUrl = (slug) =>
 
 const twitterPostContent = (summary, postUrl) => {
   // Max characters limit - link (it's always 23, no matter what) - space
-  const TWITTER_MAX_SUMMARY_LENGTH = 280 - 23 - 1;
+  const TWITTER_MAX_SUMMARY_LENGTH = 280 - 23 - 1
 
   // If it fits within the character allowance, leave it alone.
   // If not, trim it down to the the last occurance of the space
   // that fits within the bounds, and add ellipsis at the end.
-  const twitterSummary = summary.length > TWITTER_MAX_SUMMARY_LENGTH
-    ? `${summary.slice(0, summary.lastIndexOf(' ', TWITTER_MAX_SUMMARY_LENGTH - 3))}...`
-    : summary;
+  const twitterSummary =
+    summary.length > TWITTER_MAX_SUMMARY_LENGTH
+      ? `${summary.slice(0, summary.lastIndexOf(' ', TWITTER_MAX_SUMMARY_LENGTH - 3))}...`
+      : summary
 
-  return `${twitterSummary} ${postUrl}`;
+  return `${twitterSummary} ${postUrl}`
 }
 
 const postDateTemplate = {
@@ -140,7 +140,10 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                     <div className="flex flex-wrap">
                       <SocialIcon
                         kind="twitter"
-                        href={`https://twitter.com/intent/tweet?text=${twitterPostContent(summary, siteMetadata.siteUrl)}`}
+                        href={`https://twitter.com/intent/tweet?text=${twitterPostContent(
+                          summary,
+                          siteMetadata.siteUrl
+                        )}`}
                         size="6"
                       />
                       <SocialIcon
