4b35c176b1d49bcedac6b5b78f67ae2b933da851
Adam McKerlie <adammckerlie@gmail.com>
2023-04-19 21:09:55 -0400

    Update CSP policy for Replay and Plausible (#62)
    Update CSP policy

--- next.config.js
+++ next.config.js
@@ -7,13 +7,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com www.google-analytics.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com www.google-analytics.com plausible.io;
   style-src 'self' 'unsafe-inline' *.googleapis.com;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
   frame-src youtube.com www.youtube.com;
+  worker-src 'self' blob:;
+  child-src 'self' blob:;
 `
 
 const securityHeaders = [
