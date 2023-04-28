3caf022184934e70c8fd7237e08a97a540fc8e57
Steven Eubank <47563310+smeubank@users.noreply.github.com>
2023-04-28 17:28:20 +0200

    Migrating to NextJs13 (#39)
    * Migrating to NextJs13
    
    For dog fooding purposes it would be great if this was NextJS 13. For now it is Next12 (this also means React 18)
    
    Running some code mods to upgrade, let's see if it is easy to do while the blog site is young
    
    For now only did this really
    
    ```npm i next@latest react@latest react-dom@latest eslint-config-next@latest```
    ```npx @next/codemod new-link . --force```
    ```npx @next/codemod next-image-to-legacy-image . --force```
    
    happy to discuss if there was already some reasoning to not have it on v13
    
    * Create sentry.edge.config.js
    
    forgot this newbie for 13 :)
    
    * Upgrade mdx-bundler to work with React18
    
    ---------
    
    Co-authored-by: Adam McKerlie <adammckerlie@gmail.com>

--- components/Image.js
+++ components/Image.js
@@ -1,4 +1,4 @@
-import NextImage from 'next/image'
+import NextImage from 'next/legacy/image'
 
 // eslint-disable-next-line jsx-a11y/alt-text
 const Image = ({ ...rest }) => <NextImage {...rest} />
