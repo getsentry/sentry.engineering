970eadac485eba5f26d50e483c70a585ff1afac5
Adam McKerlie <adammckerlie@gmail.com>
2023-08-28 11:48:36 -0400

    Add static generation to author pages to speed it up

--- pages/about/[slug].js
+++ pages/about/[slug].js
@@ -1,12 +1,29 @@
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { getFileBySlug } from '@/lib/mdx'
 import { getPostsByAuthor } from '@/lib/mdx'
+import { getAllAuthors } from '@/lib/mdx'
 
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
   let authorDetails = await getFileBySlug('authors', [context.params.slug])
   const posts = await getPostsByAuthor(context.params.slug)
   authorDetails.posts = posts
   return { props: { authorDetails } }
+}
+
+export async function getStaticPaths() {
+  const authors = await getAllAuthors('blog')
+
+  var paths = []
+  for (let author in authors) {
+    paths.push({
+      params: { slug: author },
+    })
+  }
+
+  return {
+    paths,
+    fallback: false,
+  }
 }
 
 export default function About({ authorDetails }) {
