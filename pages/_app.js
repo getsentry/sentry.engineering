26014e002b03d31feeccd13700e6fc4014b2ca0c
Daniel Griesser <daniel.griesser.86@gmail.com>
2024-01-18 21:03:54 +0100

    feat: Add Sentry pageview metrics

--- pages/_app.js
+++ pages/_app.js
@@ -11,11 +11,17 @@ import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+import * as Sentry from '@sentry/nextjs'
+
+import { useEffect } from 'react'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    Sentry.metrics.increment('pageview', 1)
+  })
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
