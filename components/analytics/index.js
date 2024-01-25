fa37793c364beb817433a44972c6d2f86b551afa
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2024-01-24 16:05:45 -0800

    more linting

--- components/analytics/index.js
+++ components/analytics/index.js
@@ -4,9 +4,7 @@ import siteMetadata from '@/data/siteMetadata'
 const isProduction = process.env.NODE_ENV === 'production'
 
 const Analytics = () => {
-  return (
-    <>{isProduction && siteMetadata.analytics.plausibleDataDomain && <Plausible />}</>
-  )
+  return <>{isProduction && siteMetadata.analytics.plausibleDataDomain && <Plausible />}</>
 }
 
 export default Analytics
