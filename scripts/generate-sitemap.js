bca32584ed24f4c7deb5377e1464928c64bfe690
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2024-03-19 11:20:01 -0700

    include posts in `data/blog` folder

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -11,6 +11,8 @@ const siteMetadata = require('../data/siteMetadata')
     'pages/*.tsx',
     'data/blog/**/*.mdx',
     'data/blog/**/*.md',
+    'data/blog/*.mdx',
+    'data/blog/*.md',
     'public/tags/**/*.xml',
     '!pages/_*.js',
     '!pages/_*.tsx',
