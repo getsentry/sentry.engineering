10d512814a6ca498b62e379780a474bc3c8add87
Michael Hoffmann <michi@sentry.io>
2026-04-02 13:41:46 +0200

    fmt

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,10 +3,7 @@ import * as Sentry from "@sentry/astro";
 Sentry.init({
   dsn: import.meta.env.PUBLIC_SENTRY_DSN,
   sendDefaultPii: true,
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
   tracesSampleRate: 1.0,
   replaysOnErrorSampleRate: 1.0,
 });
